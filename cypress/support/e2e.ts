f104e5e28c6f91b5e74a4e71e4ef0de03585b870
Cormac Allen <cormacallen79@gmail.com>
2025-03-23 21:04:26 +0000

    initial set up for cypress

---
+++ cypress/support/e2e.ts
@@ -0,0 +1,12 @@
+// Import commands.js
+import './commands';
+
+// Cypress includes Chai assertions for BDD and TDD interface
+// https://docs.cypress.io/guides/references/bundled-tools#Chai
+// https://docs.cypress.io/guides/references/bundled-tools#Chai-jQuery
+
+// Suppress uncaught exceptions - useful for third-party libraries
+// Cypress.on('uncaught:exception', (err, runnable) => {
+//   // returning false here prevents Cypress from failing the test
+//   return false;
+// });
